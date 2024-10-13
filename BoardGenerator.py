eff07923a3b466aefc65d72038e06b0574dc43b4
Brian Lin <brianwoodlin@gmail.com>
2024-10-13 17:26:34 +0200

    Added proper mine neighbor counting

--- BoardGenerator.py
+++ BoardGenerator.py
@@ -1,8 +1,8 @@
 import random
 
-length = 30
-height = 16
-mines = 99
+length = 9
+height = 9
+mines = 10
 #generate an empty board
 grid = [[0 for i in range(length)] for j in range(height)]
 userGrid = [[9 for i in range(length)] for j in range(height)]
@@ -15,10 +15,27 @@ for i in range(mines):
     x,y = positions[i]
     grid[x][y] = 1
 
+mineCountGrid = [[0 for i in range(length + 2)] for j in range(height + 2)]
+for i in range(height):
+    for j in range(length):
+        mineCountGrid[i+1][j+1] = grid[i][j]
+neighborMine = [[0 for i in range(length)] for j in range(height)]
+for i in range(height):
+    for j in range(length):
+        neighborMine[i][j] = mineCountGrid[i + 1][j + 2] + mineCountGrid[i + 2][j + 2] + mineCountGrid[i + 2][j + 1] + mineCountGrid[i + 2][j] + mineCountGrid[i + 1][j] + mineCountGrid[i][j] + mineCountGrid[i][j + 1] + mineCountGrid[i][j + 2]
 
 #print matrix
 for row in grid:
     print(row)
+print("\n")
+
+#for row in mineCountGrid:
+#    print(row)
+#print("\n")
+
+for row in neighborMine:
+    print(row)
+print("\n")
 
 while True:
     row = int(input("Row:"))
@@ -27,15 +44,10 @@ while True:
         print("mine hit")
         break
     else:
-        userGrid[row][column] = grid[row][column + 1] + grid[row + 1][column + 1] + grid[row + 1][column] + grid[row + 1][column - 1] + grid[row][column - 1] + grid[row - 1][column -1] + grid[row - 1][column] + grid[row - 1][column + 1]
-        print("mine not hit, try again:")
-        for row in userGrid:
-            print(row)
-        print("Here are the mines")
-        #print matrix
-        for row in grid:
-            print(row)
-        print("This round finishs.")
+        userGrid[row][column] = neighborMine[row][column]
+    for row in userGrid:
+        print(row)
+    print("This round finishs.")
 
 #print user input
 for row in userGrid:
